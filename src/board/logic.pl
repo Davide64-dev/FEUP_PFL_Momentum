f743c71500a359c763046647feb1112debf88d40
Davide64-dev <davidpcu@hotmail.com>
2023-10-25 16:07:25 +0100

    small changes

--- src/board/logic.pl
+++ src/board/logic.pl
@@ -1,8 +1,20 @@
+:- consult('board.pl').
+
 nrPieces(7, 7, 8).
 nrPieces(7, 9, 10).
 nrPieces(9, 9, 12).
 
+first_element([X|_], X).
+
 putPiece(Board, X, Y, NewCell, NewBoard) :-
+        length(Board, XMax),
+        first_element(Board, Temp),
+        length(Temp, YMax),
+        X > 0,
+        X < XMax - 1,
+        Y > 0,
+        Y < YMax - 1,
+
         get_cell(Board, X, Y, Cell),
         X1 is X + 1,
         Y1 is Y + 1,
@@ -18,6 +30,7 @@ putPiece(Board, X, Y, NewCell, NewBoard) :-
         pushPiece(Board6, X2, Y2, -1, -1, Board7),
         pushPiece(Board7, X, Y2, 0, -1, Board8),
         pushPiece(Board8, X1, Y2, 1, -1, NewBoard).
+        
 
 pushPiece(Board, X, Y, IncX, IncY, NewBoard) :-
         X1 is X + IncX,
@@ -42,5 +55,5 @@ pushPiece(Board, X, Y, IncX, IncY, NewBoard) :-
         Cell1 \= white,
         pushPiece(Board, X1, Y1, IncX, IncY, NewBoard).
 
-init :- initialize_board(9, 9, _X), putPiece(_X, 3, 3, blue, _Y), putPiece(_Y, 2, 2, red,_Z), print_board(_Z).
+init :- initialize_board(9, 9, _X), putPiece(_X, 3, 3, blue, _Y), putPiece(_Y, 2, 2, red,_Z), print_board(_Z), putPiece(_Z, 8, 8, red, _W), print_board(_W).
         
