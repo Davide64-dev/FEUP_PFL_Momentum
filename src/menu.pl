d3ade93a926bc21f466c6f08be6a52477f23405e
Davide64-dev <davidpcu@hotmail.com>
2023-10-26 15:19:53 +0100

    the menu is creating the board

--- src/menu.pl
+++ src/menu.pl
@@ -1,4 +1,5 @@
 :-consult('io.pl').
+:-consult('board/board.pl').
 
 %Prints the game title
 print_title :-
@@ -11,27 +12,61 @@ print_title :-
     write('\n').
 
 
-valid_menu_options(['0', '1', '2']).
+valid_menu_options(['0', '1', '2', '3']).
 valid_submenu_options(['0', '1', '2', '3']).
+
+handle(start, '1', play).
+handle(start, '2', rules).
+handle(start, '0', exit).
+handle(rules, '0', start).
+handle(play, '1', pvp).
+handle(play, '2', pvc).
+handle(play, '3', cvc).
+
+numToBoard('1', '7 X 7').
+numToBoard('2', '7 X 9').
+numToBoard('3', '9 X 9').
+
+board_dimensions('7 X 7', 7, 7).
+board_dimensions('7 X 9', 7, 9).
+board_dimensions('9 X 9', 9, 9).
+
 
 
 %Prints the menu options
-print_menu :-
+print_menu(start, Res) :-
     print_option('1', 'Play'),
     print_option('2', 'Rules'),
     print_option('0', 'Exit'),
     write('\n'),
-    write('Select an option\n').
+    write('Select an option\n'),
+    read_option(Temp),
+    handle(start, Temp, Res).
 
+print_menu(rules, Res) :-
+    write('\n'),
+    write('Rules: \n\n'),
+    write('- Players take turns dropping one of their counters onto an empty cell of the game board.\n\n'), 
+    write('- The momentum of the dropped counter is transferred in up to eight directions through adjacent contiguous lines of counters.\n\n'),
+    write('- Counters at the end of these lines are pushed away one cell from the dropped counter.\n\n'),
+    write('- Some fall off the board and are returned to the owning players.\n\n'),
+    write('- The first player to have all of his or her counters on the board, at the end of his or her turn, wins the game.\n\n'),
+    write('\n'),
+    write('Menu: \n'),
+    print_option('0', 'Exit'),
+    read_option(Temp),
+    handle(rules, Temp, Res).
 
-%Prints the submenu options
-print_submenu :-
+print_menu(play, Res) :-
+    write('\n'),
     print_option('1', 'Human VS Human'),
     print_option('2', 'Human VS Computer'),
     print_option('3', 'Computer VS Computer'),
     print_option('0', 'Get back'),
     write('\n'),
-    write('Select an option\n').
+    write('Select a Game Type\n'),
+    read_option(Temp),
+    handle(play, Temp, Res).
 
 
 %Prints the board size options
@@ -130,21 +165,6 @@ handle_menu_option('1') :-
     print_submenu,
     read_option(SelectedOption),
     handle_submenu_option(SelectedOption). 
-   
-
-handle_menu_option('2') :-
-    write('\n'),
-    write('Rules: \n'),
-    write('Players take turns dropping one of their counters onto an empty cell of the game board.\n'), 
-    write('The momentum of the dropped counter is transferred in up to eight directions through adjacent contiguous lines of counters.\n'),
-    write('Counters at the end of these lines are pushed away one cell from the dropped counter.\n'),
-    write('Some fall off the board and are returned to the owning players.\n'),
-    write('The first player to have all of his or her counters on the board, at the end of his or her turn, wins the game.\n'),
-    write('\n'),
-    write('Menu: \n'),
-    print_menu,
-    read_option(SelectedOption),
-    handle_menu_option(SelectedOption).
 
 
 handle_menu_option('0') :-
@@ -156,6 +176,41 @@ handle_menu_option('0') :-
 % Define a predicate to run the menu.
 run_menu :-
     print_title,
-    print_menu,
-    read_option(SelectedOption),
-    handle_menu_option(SelectedOption).  
+    menu(start).
+
+
+menu(State) :-
+        State \= exit,
+        State \= pvp,
+        State \= pvc,
+        State \= cvc,
+        print_menu(State, Res),
+        menu(Res).
+
+menu(pvp) :-
+    print_board_menu,
+    read_option(Temp),
+    numToBoard(Temp, Temp1),
+    board_dimensions(Temp1, Width, Height),
+    initialize_board(Width, Height, _Board),
+    print_board(_Board).
+
+menu(pvc) :-
+    print_board_menu,
+    read_option(Temp),
+    numToBoard(Temp, Temp1),
+    board_dimensions(Temp1, Width, Height),
+    initialize_board(Width, Height, _Board),
+    print_board(_Board).
+
+menu(cvc) :-
+    print_board_menu,
+    read_option(Temp),
+    numToBoard(Temp, Temp1),
+    board_dimensions(Temp1, Width, Height),
+    initialize_board(Width, Height, _Board),
+    print_board(_Board).
+        
+
+menu(exit).
+
