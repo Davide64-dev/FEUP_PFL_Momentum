1e3e7f4e103f9b668295fe5f1b75a1690df9689d
unknown <up202108849@up.pt>
2023-11-05 00:43:09 +0000

    Added bot

--- src/bot.pl
+++ src/bot.pl
@@ -1,6 +1,5 @@
 :- use_module(library(random)).
 
-
 easyBotMove(Board, BotColor, NewBoard) :-
     % Find all empty cells on the board.
     findEmptyCells(Board, EmptyCells),
@@ -13,3 +12,32 @@ easyBotMove(Board, BotColor, NewBoard) :-
         putPiece(_Board, Row, Column, X, NewBoard)
     ).
 
+
+
+% Define the find_best_move predicate for the smart bot
+find_best_move(Board, SmartBotColor, Depth, BestMove) :-
+    findall(Move, valid_move(Board, Move), AvailableMoves),
+    best_move(Board, AvailableMoves, SmartBotColor, Depth, -9999, none, BestMove).
+
+
+% Main predicate for finding the best move
+best_move(_, [], _, _, BestScore, BestMove, BestMove) :- BestScore \= -9999.
+best_move(_, [], _, _, _, BestMove, BestMove).
+
+best_move(Board, [Move | RestMoves], SmartBotColor, Depth, CurrentBestScore, CurrentBestMove, BestMove) :-
+    make_move(Board, Move, SmartBotColor, NewBoard),
+    evaluate_board(NewBoard, SmartBotColor, Score),
+    (Depth > 0 ->
+        opponent_color(SmartBotColor, OpponentColor),
+        minimax(NewBoard, Depth, OpponentColor, ScoreDiff)
+    ; ScoreDiff = Score),
+    NewScore is Score - ScoreDiff,
+    (NewScore > CurrentBestScore ->
+        best_move(Board, RestMoves, SmartBotColor, Depth, NewScore, Move, BestMove)
+    ; best_move(Board, RestMoves, SmartBotColor, Depth, CurrentBestScore, CurrentBestMove, BestMove)
+    ).
+
+% Minimax search with alpha-beta pruning
+minimax(Board, Depth, Player, Score) :-
+    (Depth == 0 ; game_over(Board)),
+    evaluate_board(Board, Player, Score).
